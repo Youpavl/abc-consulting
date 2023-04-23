fb9dc376cc15ee7be567955cce79be038d0c396e
Youpavl <129831608+Youpavl@users.noreply.github.com>
2023-04-23 09:57:20 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
 $(document).ready(function () {
 
 })
-$('.menu a[href="https://youpavl.github.io/abc-consulting/"]').addClass('font-weight: 500');
+$('.menu').addClass('font-weight: 500');
 
 const messageInput = document.querySelector('.form__message');
 
